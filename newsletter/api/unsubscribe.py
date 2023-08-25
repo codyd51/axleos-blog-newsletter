4df4333b911d6f79be72b1e9914e7e5352e7fa8c
Phillip Tennen <pt@datatheorem.io>
2023-08-25 19:06:27 +0100

    /unsubscribe is accessible via GET

--- newsletter/api/unsubscribe.py
+++ newsletter/api/unsubscribe.py
@@ -4,12 +4,10 @@ import datetime
 
 import falcon
 from pydantic import BaseModel
-import google.api_core.exceptions
 
 from models.subscribed_users import SubscribedUser
 from models.subscribed_users import get_subscribed_users_collection
-from newsletter.utils.api import parse_json_body
-
+from utils.api import parse_get_parameters
 
 _logger = logging.getLogger(__name__)
 
@@ -19,8 +17,10 @@ class UnsubscribeEmailRequest(BaseModel):
 
 
 class UnsubscribeEmailResource:
-    def on_post(self, request: falcon.Request, response: falcon.Response) -> None:
-        unsubscribe_info = parse_json_body(request, UnsubscribeEmailRequest)
+    def on_get(self, request: falcon.Request, response: falcon.Response) -> None:
+        # PT: This endpoint is implemented with a GET so that it can be embedded as a link, though conceptually a
+        # different method would be more appropriate.
+        unsubscribe_info = parse_get_parameters(request, UnsubscribeEmailRequest)
         client_ip = request.remote_addr
         _logger.info(f'Unsubscribing email on behalf of {client_ip}: {unsubscribe_info}')
 
--- newsletter/utils/api.py
+++ newsletter/utils/api.py
@@ -13,3 +13,8 @@ def parse_json_body(request: falcon.Request, body_shape: Type[_BaseModelT]) -> _
     request_json = json.loads(content)
     parsed_message = body_shape.parse_obj(request_json)
     return parsed_message
+
+
+def parse_get_parameters(request: falcon.Request, body_shape: Type[_BaseModelT]) -> _BaseModelT:
+    parsed_message = body_shape.parse_obj(request.params)
+    return parsed_message
