b17825c8be74d96aa4f1fb71ac2c97d4f8285b31
Phillip Tennen <pt@datatheorem.io>
2023-08-25 18:27:29 +0100

    Implement a new base for credentials backed by a secrets file

---
+++ newsletter/utils/credentials_file.py
@@ -0,0 +1,17 @@
+import json
+from abc import ABC
+from pathlib import Path
+from typing import ClassVar
+from typing import Self
+
+from pydantic import BaseModel
+
+
+class CredentialsBackedByFile(ABC, BaseModel):
+    # Must be set by subclasses
+    _CREDENTIALS_FILE: ClassVar[Path]
+
+    @classmethod
+    def get_default(cls) -> Self:
+        raw_json = json.loads(cls._CREDENTIALS_FILE.read_bytes())
+        return cls.parse_obj(raw_json)
