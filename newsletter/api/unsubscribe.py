27e31ad0bc7e9cbb0eaa956e60ec45ac5a61ce47
Phillip Tennen <pt@datatheorem.io>
2023-08-25 19:07:13 +0100

    Unsubscribing sends a confirmation email

--- newsletter/api/unsubscribe.py
+++ newsletter/api/unsubscribe.py
@@ -1,6 +1,5 @@
 import json
 import logging
-import datetime
 
 import falcon
 from pydantic import BaseModel
@@ -8,6 +7,7 @@ from pydantic import BaseModel
 from models.subscribed_users import SubscribedUser
 from models.subscribed_users import get_subscribed_users_collection
 from utils.api import parse_get_parameters
+from utils.email import send_email
 
 _logger = logging.getLogger(__name__)
 
@@ -25,8 +25,20 @@ class UnsubscribeEmailResource:
         _logger.info(f'Unsubscribing email on behalf of {client_ip}: {unsubscribe_info}')
 
         document_ref = get_subscribed_users_collection().document(unsubscribe_info.email)
-        if document_ref.get().exists:
+        document_snap = document_ref.get()
+        if document_snap.exists:
+            user_record = SubscribedUser.from_snapshot(document_snap)
             document_ref.delete()
+
+            # Inform the user they've been successfully unsubscribed
+            send_email(
+                to_user=user_record,
+                subject="You've been unsubscribed from https://axleos.com/blog",
+                template_name="unsubscribe.html.jinja2",
+                # No need to include an unsubscribe button
+                should_include_unsubscribe_button=False,
+            )
+
         else:
             _logger.error(f"Tried to unsubscribe a user that's not subscribed: {unsubscribe_info}")
 
