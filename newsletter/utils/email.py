0958d514941e346e458e81ea5fbe21360b50f637
Phillip Tennen <pt@datatheorem.io>
2023-08-26 11:05:39 +0100

    Bugfix when extra_jinja_context is None

--- newsletter/utils/email.py
+++ newsletter/utils/email.py
@@ -24,16 +24,15 @@ def send_email(
     subscription_duration = now.replace(tzinfo=None) - to_user.date_created.replace(tzinfo=None)
 
     context = TemplateContext(
-        # TODO(PT): Dynamically generate these colors
-        background_color="rgb(254, 255, 252)",
-        border_color="rgb(197, 198, 195)",
         generated_at=now.strftime("%B %d, %Y at %H:%M"),
         should_include_unsubscribe_button=should_include_unsubscribe_button,
         should_include_user_metadata=True,
         user_email=to_user.user_email,
         subscription_duration=format_timedelta(subscription_duration),
     )
-    full_context = {**asdict(context), **extra_jinja_context}
+    full_context = asdict(context)
+    if extra_jinja_context:
+        full_context.update(extra_jinja_context)
     email_content = EMAIL_JINJA_ENVIRONMENT.get_template(template_name).render(full_context)
 
     from_sender = From("backend@axleos.com", "axleOS.com backend")
